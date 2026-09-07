be8fb3c003b202543e4aa0488b928638cae3d027
DynastyOSX <derian.acosta2022@gmail.com>
2026-09-07 01:10:31 -0500

    Decompile func_8009AE08

--- src/core2/baphysics.c
+++ src/core2/baphysics.c
@@ -19,7 +19,84 @@ s32 func_8009AE00()
     return 0xC4;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/baphysics/func_8009AE08.s")
+void func_8009AE08(PlayerState *self)
+{
+    f32 sp_3c;
+    f32 sp_4c[3];
+    f32 sp_40[3];
+    f32 sp_58[3];
+    f32 sp_64[3];
+    f32 sp_70[3];
+    f32 sp_7c[3];
+
+    func_800EEB9C(
+        self->baphysics + 0x1C,
+        self->baphysics->unk40,
+        self->baphysics->unk3C * self->baphysics->unk50
+    );
+
+    sp_58[0] = self->baphysics->unk10;
+    sp_58[1] = 0.0f;
+    sp_58[2] = self->baphysics->unk18;
+
+    func_800EE7F8(self->baphysics + 0x1C, sp_58);
+
+    sp_3c = 0.0f;
+
+    if (func_80096518(self) == 0)
+        goto end;
+
+    func_800963C0(self, sp_4c);
+    func_800EF410(sp_40, sp_58);
+    func_800EEAA4(sp_40, sp_4c);
+
+    sp_3c = func_800A3378(self);
+
+    if (sp_3c == 0.0f)
+        goto end;
+
+    if (sp_3c < 0.0f) {
+        if (func_8008E1C4(self)) {
+            sp_3c = func_800F10B4(
+                sp_3c,
+                0.0f,
+                1.0f,
+                sp_3c * 0.5f,
+                -1.0f
+            );
+        } else {
+            sp_3c *= 0.5f;
+        }
+    } else {
+        func_800EF334(
+            sp_58,
+            1.0f + sp_3c * D_801252A0
+        );
+    }
+
+end:
+    func_800EF3DC(sp_7c, sp_70);
+    func_800EF334(sp_7c, func_800D8FF8() * 30.0f);
+
+    self->baphysics->unk14 +=
+        self->baphysics->unk34 * func_800D8FF8();
+
+    if (self->baphysics->unk14 < self->baphysics->unk38)
+        self->baphysics->unk14 = self->baphysics->unk38;
+
+    func_800EFA20(
+        self->baphysics + 0x28,
+        self->baphysics + 0x10,
+        func_800D8FF8()
+    );
+
+    func_800EF04C(
+        self->baphysics + 0x04,
+        self->baphysics + 0x28
+    );
+
+    func_800F2168(self->baphysics + 0x10, 0.0001f);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/baphysics/func_8009B08C.s")
 
